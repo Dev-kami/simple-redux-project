b2b01289daef76f5d246199e67392556694aca95
Obasi Chizitere <blossomobasi2@gmail.com>
2024-05-10 12:18:26 +0100

    Initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Provider } from "react-redux";
-import { store } from "@/state/store";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,9 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Provider store={store}>
-        <body className={inter.className}>{children}</body>
-      </Provider>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,7 @@ import React from "react";
 const HomePage = () => {
   return (
     <div className="flex items-center justify-center h-screen">
-      <h1 className="text-3xl">
-        Welcome to <span className="text-blue-500">React</span> +{" "}
-        <span className="text-red-500">Tailwind</span> +{" "}
-        <span className="text-green-500">TypeScript</span> Starter
-      </h1>
+      <h1>Counter</h1>
     </div>
   );
 };
