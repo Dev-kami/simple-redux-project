2ad31d97d839cda690a218a65f6e973856fdfd43
Obasi Chizitere <blossomobasi2@gmail.com>
2024-05-10 13:10:05 +0100

    Setup and (increment and decrement functionality)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,36 @@
+"use client";
+
 import React from "react";
 
+import { decrement, increment } from "@/lib/counter/counter";
+import { useAppSelector } from "@/lib/hooks";
+import { useDispatch } from "react-redux";
+
 const HomePage = () => {
+  const count = useAppSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
+
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex items-center flex-col gap-y-5 justify-center h-screen">
       <h1>Counter</h1>
+
+      <div className="space-x-5">
+        <button
+          onClick={() => dispatch(decrement())}
+          aria-label="Decrement value"
+        >
+          Decrement
+        </button>
+
+        <span>{count}</span>
+
+        <button
+          onClick={() => dispatch(increment())}
+          aria-label="Increment value"
+        >
+          Increment
+        </button>
+      </div>
     </div>
   );
 };
---
+++ src/lib/counter/counter.ts
@@ -0,0 +1,32 @@
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+
+type CounterState = {
+  count: number;
+};
+
+const initialState: CounterState = {
+  count: 0,
+};
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.count += 1;
+    },
+    decrement: (state) => {
+      state.count -= 1;
+    },
+    incrementByAmount: (state, action: PayloadAction<number>) => {
+      state.count += action.payload;
+    },
+    decrementByAmount: (state, action: PayloadAction<number>) => {
+      state.count -= action.payload;
+    },
+  },
+});
+
+export default counterSlice.reducer;
+export const { increment, decrement, incrementByAmount, decrementByAmount } =
+  counterSlice.actions;
