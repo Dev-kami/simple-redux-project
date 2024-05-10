d67fa228de3712b64fc11208d437e9bc98f3ed51
Obasi Chizitere <blossomobasi2@gmail.com>
2024-05-10 13:24:21 +0100

    Remove incrementByAmount and decrementbyAmount

--- src/lib/counter/counter.ts
+++ src/lib/counter/counter.ts
@@ -18,15 +18,8 @@ const counterSlice = createSlice({
     decrement: (state) => {
       state.count -= 1;
     },
-    incrementByAmount: (state, action: PayloadAction<number>) => {
-      state.count += action.payload;
-    },
-    decrementByAmount: (state, action: PayloadAction<number>) => {
-      state.count -= action.payload;
-    },
   },
 });
 
 export default counterSlice.reducer;
-export const { increment, decrement, incrementByAmount, decrementByAmount } =
-  counterSlice.actions;
+export const { increment, decrement } = counterSlice.actions;
